5d164482eabf07db7e46a559501b0b3097b24133
lpm11 <lpm11r@gmail.com>
2012-02-18 03:20:25 +0900

    Now log4j is configurable.

--- src/main/java/AppBase.java
+++ src/main/java/AppBase.java
@@ -2,6 +2,8 @@ import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import org.msgpack.rpc.Server;
 import org.msgpack.rpc.loop.EventLoop;
+import org.apache.log4j.xml.DOMConfigurator;
+import java.io.File;
 
 public class AppBase {
      /**
@@ -11,7 +13,13 @@ public class AppBase {
         OptionParser optparse = new OptionParser();
         optparse.accepts("port","Listen port.").withRequiredArg();
         optparse.accepts("lm","Language model file name.").withRequiredArg();
+        optparse.accepts("log4j","log4j configuration file.").withRequiredArg();
         OptionSet opt = optparse.parse(args);
+
+        File file = new File("log4j.xml");
+        if (opt.has("log4j") || file.exists()) {
+        	DOMConfigurator.configure(opt.has("log4j") ? (String)opt.valueOf("log4j"):"log4j.xml");
+        }
 
         KylmServer ks = new KylmServer((String)opt.valueOf("lm"));
     	EventLoop loop = EventLoop.defaultEventLoop();
